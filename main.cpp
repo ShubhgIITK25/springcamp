90822175d4cb8cf72180b278baf2eb70026b2103
Yatin Bhojwani <yatinbhojwani@gmail.com>
2026-01-21 21:08:24 +0530

    hello world

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); // Faster input/output
+using namespace std;
+
+
+
+int main() {
+    fastio;
+    cout << "hello world ";
+    return 0;
+}
